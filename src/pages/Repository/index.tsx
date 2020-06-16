9a4b8dd3c9bb4d1094327c5cd8b34b8f3e3c8ef0
Igor Tuag <igortuag@gmail.com>
2020-06-16 07:36:10 -0300

    Navegando entre rotas

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
-const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+interface IRepositoryParams {
+  repository: string;
 }
 
-export default Repository
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<IRepositoryParams>();
+
+  return <h1>Repository: {params.repository}</h1>;
+};
+
+export default Repository;
